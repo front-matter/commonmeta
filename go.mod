c1136da60b91ed7d52fbca491ce850fa2338770a
Martin Fenner <martin@front-matter.io>
2024-04-05 17:57:14 +0200

    more json schema tests

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require github.com/xeipuuv/gojsonschema v1.2.0
 require (
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
 )
--- schemautils/schemautils_test.go
+++ schemautils/schemautils_test.go
@@ -4,10 +4,13 @@ import (
 	"commonmeta/metadata"
 	"commonmeta/schemautils"
 	"encoding/json"
+
 	"fmt"
 	"log"
 	"os"
 	"testing"
+
+	"sigs.k8s.io/yaml"
 )
 
 func TestJSONSchemaErrors(t *testing.T) {
@@ -60,8 +63,11 @@ func TestJSONSchemaErrorsTestdata(t *testing.T) {
 	}
 
 	testCases := []testCase{
+		{meta: "journal_article.commonmeta.json", schema: "commonmeta_v0.12", want: 2},
 		{meta: "citeproc.json", schema: "csl-data", want: 0},
 		{meta: "datacite.json", schema: "datacite-v4.5", want: 3},
+		{meta: "datacite-instrument.json", schema: "datacite-v4.5", want: 27},
+		{meta: "datacite_software_version.json", schema: "datacite-v4.5", want: 7},
 	}
 	for _, tc := range testCases {
 		data, err := os.ReadFile("../testdata/" + tc.meta)
@@ -69,6 +75,34 @@ func TestJSONSchemaErrorsTestdata(t *testing.T) {
 			fmt.Print(err)
 		}
 		result := schemautils.JSONSchemaErrors(data, tc.schema)
+		got := len(result.Errors())
+		if tc.want != got {
+			t.Errorf("want %d, got %d", tc.want, got)
+		}
+	}
+}
+
+func TestJSONSchemaErrorsTestdataYAML(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		meta   string
+		schema string
+		want   int
+	}
+
+	testCases := []testCase{
+		{meta: "CITATION.cff", schema: "cff_v1.2.0", want: 0},
+	}
+	for _, tc := range testCases {
+		data, err := os.ReadFile("../testdata/" + tc.meta)
+		if err != nil {
+			fmt.Print(err)
+		}
+		YAMLdata, err := yaml.YAMLToJSON(data)
+		if err != nil {
+			fmt.Print(err)
+		}
+		result := schemautils.JSONSchemaErrors(YAMLdata, tc.schema)
 		got := len(result.Errors())
 		if tc.want != got {
 			t.Errorf("want %d, got %d", tc.want, got)
