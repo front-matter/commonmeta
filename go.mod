26d1a380bd1d688bd2e868e5eb5f961078236a15
Martin Fenner <martin@front-matter.io>
2024-12-31 11:03:39 +0100

    remove gojsonschema

--- go.mod
+++ go.mod
@@ -10,7 +10,6 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/microcosm-cc/bluemonday v1.0.27
 	github.com/spf13/cobra v1.8.1
-	github.com/xeipuuv/gojsonschema v1.2.0
 	golang.org/x/time v0.8.0
 	gopkg.in/yaml.v3 v3.0.1
 	mvdan.cc/xurls/v2 v2.5.0
@@ -24,8 +23,6 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
-	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	golang.org/x/mod v0.21.0 // indirect
 	golang.org/x/net v0.31.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
--- schemautils/schemautils.go
+++ schemautils/schemautils.go
@@ -5,23 +5,23 @@ import (
 	"embed"
 	"fmt"
 	"log"
-	"os"
 	"path/filepath"
 	"slices"
 
 	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/cuecontext"
 	"cuelang.org/go/encoding/json"
+	"cuelang.org/go/encoding/jsonschema"
 )
 
-// JSONSchemas is the embedded JSON Schema files.
+// Schemas is the embedded JSON Schema files.
 //
-//go:embed schemas/*.cue
+//go:embed schemas/*.json
 var Schemas embed.FS
 
 const schemaVersion = "commonmeta_v0.15"
 
-// SchemaErrors validates a JSON document against a Cue Schema file.
+// SchemaErrors validates a JSON document against a JSON Schema using Cue.
 func SchemaErrors(document []byte, schema ...string) error {
 
 	// If no schema is provided, default to const schema_version
@@ -30,36 +30,49 @@ func SchemaErrors(document []byte, schema ...string) error {
 	}
 	s := schema[len(schema)-1]
 
-	// Cue Schema files stored locally to validate against
+	// JSON Schema files stored locally to validate against
 	schemata := []string{schemaVersion, "datacite-v4.5", "crossref-v0.2", "csl-data", "cff_v1.2.0", "invenio-rdm-v0.1"}
 	if !slices.Contains(schemata, s) {
 		log.Fatalf("Schema %s not found", s)
 	}
 	dir := "schemas"
-	data, err := Schemas.ReadFile(filepath.Join(dir, s+".cue"))
+	schemaPath := filepath.Join(dir, s+".json")
+	schemaFile, err := Schemas.ReadFile(schemaPath)
 	if err != nil {
-		ex, err := os.Executable()
-		if err != nil {
-			panic(err)
-		}
-		exPath := filepath.Dir(ex)
-		fmt.Println(exPath)
 		fmt.Print(err)
 	}
-	cueSource := string(data)
-	ctx := cuecontext.New()
 
-	// Build the schema
-	cueSchema := ctx.CompileString(cueSource).LookupPath(cue.ParsePath("schema"))
-	fmt.Println(cueSchema)
-	// Load the JSON file specified (the program's sole argument) as a CUE value
-	dataExpr, err := json.Extract("_", document)
+	ctx := cuecontext.New()
+	schemaJsonAst, err := json.Extract(schemaPath, schemaFile)
 	if err != nil {
 		log.Fatal(err)
 	}
-	dataAsCUE := ctx.BuildExpr(dataExpr)
+	schemaJson := ctx.BuildExpr(schemaJsonAst)
 
-	// Validate the JSON data using the schema
-	unified := cueSchema.Unify(dataAsCUE)
-	return unified.Validate()
+	// Extract JSON Schema from the JSON
+	schemaAst, err := jsonschema.Extract(schemaJson, &jsonschema.Config{
+		Strict: false,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Build a cue.Value of the schema
+	cueSchema := ctx.BuildFile(schemaAst)
+
+	// Load the data JSON
+	dataAst, err := json.Extract(".", document)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Build a cue.Value of the data
+	cueData := ctx.BuildExpr(dataAst)
+
+	// Unify the schema and data
+	res := cueSchema.Unify(cueData)
+
+	// Validate whether the combined (unified) result has errors or not.
+	err = res.Validate(cue.Concrete(true))
+	return err
 }
