f3f47336c9cc72ec84db40093e5a53e80119ae0e
Martin Fenner <martin@front-matter.io>
2025-02-21 02:46:42 +0000

    include datacite external doi prefix used by rogue scholar

--- doiutils/doiutils.go
+++ doiutils/doiutils.go
@@ -197,6 +197,7 @@ func IsRogueScholarDOI(doi string, ra string) bool {
 		"10.59351",
 	}
 	var rogueScholarDatacitePrefixes = []string{
+		"10.5438",
 		"10.34732",
 		"10.57689",
 		// "10.83132",
