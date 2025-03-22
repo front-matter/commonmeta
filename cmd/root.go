063aa49457d64078e552d718a837886c2f02a921
Martin Fenner <martin@front-matter.io>
2025-03-22 12:59:53 +0100

    more crossrefxml citation type mappings

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,7 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "commonmeta",
-	Version: "v0.14.8",
+	Version: "v0.14.9",
 	Short:   "Convert scholarly metadata between formats",
 	Long: `Convert scholarly metadata between formats. Currently
 supported formats include Crossref, DataCite, Schema.org, CSL, InvenioRDM,
