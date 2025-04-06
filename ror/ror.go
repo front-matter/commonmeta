cbf01b61d0188437b0383e0b0970b6fb533ec8cc
Martin Fenner <martin@front-matter.io>
2025-04-06 17:54:01 +0200

    extract ror id and name from commonmeta and ror data dump

--- ror/ror.go
+++ ror/ror.go
@@ -8,10 +8,13 @@ import (
 	"log"
 	"os"
 	"path"
+	"path/filepath"
 	"slices"
 
 	"gopkg.in/yaml.v3"
 
+	"github.com/front-matter/commonmeta/commonmeta"
+	"github.com/front-matter/commonmeta/fileutils"
 	"github.com/front-matter/commonmeta/utils"
 )
 
@@ -19,6 +22,16 @@ import (
 type ROR struct {
 	ID    string `json:"id"`
 	Names []Name `json:"names"`
+	Admin struct {
+		Created struct {
+			Date          string `json:"date"`
+			SchemaVersion string `json:"schema_version"`
+		} `json:"created"`
+		LastModified struct {
+			Date          string `json:"date"`
+			SchemaVersion string `json:"schema_version"`
+		} `json:"last_modified"`
+	}
 }
 
 // Content represents the full ROR metadata record.
@@ -31,16 +44,6 @@ type Content struct {
 	Relationships []Relationship `json:"relationships"`
 	Status        string         `json:"status"`
 	Types         []string       `json:"types"`
-	Admin         struct {
-		Created struct {
-			Date          string `json:"date"`
-			SchemaVersion string `json:"schema_version"`
-		}
-		LastModified struct {
-			Date          string `json:"date"`
-			SchemaVersion string `json:"schema_version"`
-		}
-	}
 }
 
 // InvenioRDM represents the ROR metadata record in InvenioRDM format.
@@ -95,6 +98,27 @@ type Relationship struct {
 	ID    string `json:"id"`
 }
 
+// RORVersions contains the ROR versions and their release dates, published on Zenodo.
+// The ROR version is the first part of the filename, e.g., v1.63-2025-04-03-ror-data_schema_v2.json
+// Beginning with release v1.45 on 11 April 2024, data releases contain JSON and CSV files formatted
+// according to both schema v1 and schema v2. Version 2 files have _schema_v2 appended to the end of
+// the filename, e.g., v1.45-2024-04-11-ror-data_schema_v2.json.
+var RORVersions = map[string]string{
+	"v1.50": "2024-07-29",
+	"v1.51": "2024-08-21",
+	"v1.52": "2024-09-16",
+	"v1.53": "2023-10-14",
+	"v1.54": "2024-10-21",
+	"v1.55": "2024-10-31",
+	"v1.56": "2024-11-19",
+	"v1.58": "2024-12-11",
+	"v1.59": "2025-01-23",
+	"v1.60": "2025-02-27",
+	"v1.61": "2025-03-18",
+	"v1.62": "2025-03-27",
+	"v1.63": "2025-04-03",
+}
+
 // LoadAll loads the metadata for a list of organizations from a ROR JSON file
 func LoadAll(filename string) ([]ROR, error) {
 	var data []ROR
@@ -103,10 +127,6 @@ func LoadAll(filename string) ([]ROR, error) {
 
 	extension := path.Ext(filename)
 	if extension == ".json" {
-		extension := path.Ext(filename)
-		if extension != ".json" {
-			return data, errors.New("invalid file extension")
-		}
 		file, err := os.Open(filename)
 		if err != nil {
 			return data, errors.New("error reading file")
@@ -118,6 +138,8 @@ func LoadAll(filename string) ([]ROR, error) {
 		if err != nil {
 			return data, err
 		}
+	} else if extension != ".json" {
+		return data, errors.New("invalid file extension")
 	} else {
 		return data, errors.New("unsupported file format")
 	}
@@ -135,6 +157,7 @@ func Read(content Content) (ROR, error) {
 
 	data.ID = content.ID
 	data.Names = content.Names
+	data.Admin.LastModified.Date = content.Admin.LastModified.Date
 
 	return data, nil
 }
@@ -150,6 +173,48 @@ func ReadAll(content []Content) ([]ROR, error) {
 		data = append(data, d)
 	}
 	return data, nil
+}
+
+// ExtractAll extracts ROR metadata from a JSON file in commonmeta format.
+func ExtractAll(content []commonmeta.Data) ([]byte, error) {
+	var data []InvenioRDM
+	var extracted []InvenioRDM
+	var ids []string
+	var err error
+
+	// Load the ROR metadata from the embedded ZIP file with all ROR records
+	filename := filepath.Join("ror", "affiliations_ror.yaml.zip")
+	out, err := fileutils.ReadZIPFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	err = yaml.Unmarshal(out, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract ROR IDs from the content
+	for _, v := range content {
+		if len(v.Contributors) > 0 {
+			for _, c := range v.Contributors {
+				if len(c.Affiliations) > 0 {
+					for _, a := range c.Affiliations {
+						if a.ID != "" && !slices.Contains(ids, a.ID) {
+							id, _ := utils.ValidateROR(a.ID)
+							idx := slices.IndexFunc(data, func(d InvenioRDM) bool { return d.ID == id })
+							if idx != -1 {
+								ids = append(ids, a.ID)
+								extracted = append(extracted, data[idx])
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	output, err := yaml.Marshal(extracted)
+	return output, err
 }
 
 // Convert converts ROR metadata into InvenioRDM format.
@@ -182,24 +247,26 @@ func Write(data ROR) ([]byte, error) {
 	return output, err
 }
 
-// WriteAll writes a list of ROR metadata to InvenioRDM YAML format.
-func WriteAll(list []ROR, input string) (string, error) {
+// WriteAll writes a list of ROR metadata in InvenioRDM YAML format.
+func WriteAll(list []ROR, to string) ([]byte, error) {
 	var inveniordmList []InvenioRDM
+	var err error
+	var output []byte
+
+	if to != "inveniordm" {
+		return output, errors.New("unsupported output format")
+	}
 
 	for _, data := range list {
 		inveniordm, err := Convert(data)
 		if err != nil {
 			fmt.Println(err)
 		}
-		inveniordmList = append(inveniordmList, inveniordm)
-	}
-	output, err := yaml.Marshal(inveniordmList)
-	if err != nil {
-		fmt.Println(err)
+		if inveniordm.ID != "" {
+			inveniordmList = append(inveniordmList, inveniordm)
+		}
 	}
 
-	filename := path.Base("affiliations_ror.yaml")
-	output = append([]byte("# file generated from "+input+"\n\n"), output...)
-	err = os.WriteFile(filename, output, 0644)
-	return filename, err
+	output, err = yaml.Marshal(inveniordmList)
+	return output, err
 }
