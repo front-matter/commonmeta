ea5ddfaa3a8fd518a66101f6dcb397c5593c5a62
Martin Fenner <martin@front-matter.io>
2025-04-08 18:55:07 +0200

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,7 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "commonmeta",
-	Version: "v0.17.7",
+	Version: "v0.17.8",
 	Short:   "Convert scholarly metadata between formats",
 	Long: `Convert scholarly metadata between formats. Currently
 supported formats include Crossref, DataCite, Schema.org, CSL, InvenioRDM,
