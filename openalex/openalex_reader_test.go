27b95489902d643413163558c500a6354b245e56
Martin Fenner <martin@front-matter.io>
2025-05-11 17:44:10 +0200

    added openalex tests

--- openalex/openalex_reader_test.go
+++ openalex/openalex_reader_test.go
@@ -1,10 +1,16 @@
 package openalex_test
 
 import (
+	"encoding/json"
 	"net/url"
+	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 	"testing"
 
+	"github.com/front-matter/commonmeta/commonmeta"
+	"github.com/front-matter/commonmeta/doiutils"
 	"github.com/front-matter/commonmeta/openalex"
 
 	"github.com/google/go-cmp/cmp"
@@ -42,56 +48,54 @@ func TestGet(t *testing.T) {
 	}
 }
 
-// func TestFetch(t *testing.T) {
-// 	t.Parallel()
-// 	type testCase struct {
-// 		name string
-// 		id   string
-// 	}
+func TestFetch(t *testing.T) {
+	t.Parallel()
+	type testCase struct {
+		name string
+		id   string
+	}
 
-// 	testCases := []testCase{
-// 		{name: "test doi", id: "https://doi.org/10.5555/12345678"},
-// 		{name: "journal article with data citation", id: "https://doi.org/10.7554/elife.01567"},
-// 		{name: "posted content", id: "https://doi.org/10.1101/097196"},
-// 		{name: "book", id: "https://doi.org/10.1017/9781108348843"},
-// 		{name: "book chapter", id: "https://doi.org/10.1007/978-3-662-46370-3_13"},
-// 		{name: "proceedings article", id: "https://doi.org/10.1145/3448016.3452841"},
-// 		{name: "dataset", id: "https://doi.org/10.2210/pdb4hhb/pdb"},
-// 		{name: "component", id: "https://doi.org/10.1371/journal.pmed.0030277.g001"},
-// 		{name: "peer review", id: "https://doi.org/10.7554/elife.55167.sa2"},
-// 		{name: "blog post", id: "https://doi.org/10.59350/2shz7-ehx26"},
-// 		{name: "dissertation", id: "https://doi.org/10.14264/uql.2020.791"},
-// 		{name: "with ror id", id: "https://doi.org/10.1364/oe.490112"},
-// 		{name: "archived", id: "10.5694/j.1326-5377.1943.tb44329.x"},
-// 	}
-// 	r := openalex.NewReader("info@front-matter.io")
-// 	for _, tc := range testCases {
-// 		got, err := r.Fetch(tc.id)
-// 		if err != nil {
-// 			t.Errorf("OpenAlex Metadata(%v): error %v", tc.id, err)
-// 		}
-// 		// read json file from testdata folder and convert to Data struct
-// 		doi, ok := doiutils.ValidateDOI(tc.id)
-// 		if !ok {
-// 			t.Fatal("invalid doi")
-// 		}
-// 		filename := strings.ReplaceAll(doi, "/", "_") + ".json"
-// 		filepath := filepath.Join("testdata", filename)
-// 		bytes, err := os.ReadFile(filepath)
-// 		if err != nil {
-// 			t.Fatal(err)
-// 		}
+	testCases := []testCase{
+		{name: "test doi", id: "https://doi.org/10.5555/12345678"},
+		{name: "journal article with data citation", id: "https://doi.org/10.7554/elife.01567"},
+		{name: "posted content", id: "https://doi.org/10.1101/097196"},
+		// {name: "book", id: "https://doi.org/10.1017/9781108348843"},
+		// {name: "book chapter", id: "https://doi.org/10.1007/978-3-662-46370-3_13"},
+		// {name: "proceedings article", id: "https://doi.org/10.1145/3448016.3452841"},
+		{name: "peer review", id: "https://doi.org/10.7554/elife.55167.sa2"},
+		{name: "blog post", id: "https://doi.org/10.59350/2shz7-ehx26"},
+		{name: "dissertation", id: "https://doi.org/10.14264/uql.2020.791"},
+		// {name: "with ror id", id: "https://doi.org/10.1364/oe.490112"},
+		// {name: "archived", id: "10.5694/j.1326-5377.1943.tb44329.x"},
+	}
+	r := openalex.NewReader("info@front-matter.io")
+	for _, tc := range testCases {
+		got, err := r.Fetch(tc.id)
+		if err != nil {
+			t.Errorf("OpenAlex Metadata(%v): error %v", tc.id, err)
+		}
+		// read json file from testdata folder and convert to Data struct
+		doi, ok := doiutils.ValidateDOI(tc.id)
+		if !ok {
+			t.Fatal("invalid doi")
+		}
+		filename := strings.ReplaceAll(doi, "/", "_") + ".json"
+		filepath := filepath.Join("testdata", filename)
+		bytes, err := os.ReadFile(filepath)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-// 		var want commonmeta.Data
-// 		err = json.Unmarshal(bytes, &want)
-// 		if err != nil {
-// 			t.Fatal(err)
-// 		}
-// 		if diff := cmp.Diff(want, got); diff != "" {
-// 			t.Errorf("Fetch (%s) mismatch (-want +got):\n%s", tc.id, diff)
-// 		}
-// 	}
-// }
+		var want commonmeta.Data
+		err = json.Unmarshal(bytes, &want)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if diff := cmp.Diff(want, got); diff != "" {
+			t.Errorf("Fetch (%s) mismatch (-want +got):\n%s", tc.id, diff)
+		}
+	}
+}
 
 func TestQueryURL(t *testing.T) {
 	t.Parallel()
