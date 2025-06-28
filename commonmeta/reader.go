0c0a5fd4182b9ce459211e6a20638dae937f5e50
Martin Fenner <martin@front-matter.io>
2025-06-28 10:38:30 +0200

    use different token

--- commonmeta/reader.go
+++ commonmeta/reader.go
@@ -21,7 +21,7 @@ func NewReader(r io.Reader) *Reader {
 	}
 }
 
-const Version = "v0.28.0"
+const Version = "v0.29.0"
 
 // ContributorRoles list of contributor roles defined in commonmeta schema.
 //
