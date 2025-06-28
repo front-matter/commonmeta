46bc93af4af52fe80ca6465f93c04713416f90c0
Martin Fenner <martin@front-matter.io>
2025-06-28 10:22:47 +0200

    Update reader.go

--- commonmeta/reader.go
+++ commonmeta/reader.go
@@ -21,7 +21,7 @@ func NewReader(r io.Reader) *Reader {
 	}
 }
 
-const Version = "v0.27.0"
+const Version = "v0.28.0"
 
 // ContributorRoles list of contributor roles defined in commonmeta schema.
 //
